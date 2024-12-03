bdd154a67aeb18546f0d82a545d5cb56ca43e1dd
root <root@ip-172-31-24-213.ec2.internal>
2024-12-03 16:06:16 +0000

    committed terraform file in local repo

--- vpc.tf
+++ vpc.tf
@@ -1,47 +1,47 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "one" {
-tags = {
-Name = "terraform-vpc"
-}
-instance_tenancy = "default"
-cidr_block = "100.0.0.0/16"
-enable_dns_hostnames = "true"
+  tags = {
+    Name = "terraform-vpc"
+  }
+  instance_tenancy     = "default"
+  cidr_block           = "100.0.0.0/16"
+  enable_dns_hostnames = "true"
 }
 
 resource "aws_subnet" "two" {
-tags = {
-Name = "terraform-subnet"
-}
-vpc_id = aws_vpc.one.id
-availability_zone = "us-east-1c"
-cidr_block = "100.0.0.0/24"
-map_public_ip_on_launch = "true"
+  tags = {
+    Name = "terraform-subnet"
+  }
+  vpc_id                  = aws_vpc.one.id
+  availability_zone       = "us-east-1c"
+  cidr_block              = "100.0.0.0/24"
+  map_public_ip_on_launch = "true"
 }
 
 resource "aws_internet_gateway" "three" {
-tags = {
-Name = "terraform-internetgateway"
-}
-vpc_id = aws_vpc.one.id
+  tags = {
+    Name = "terraform-internetgateway"
+  }
+  vpc_id = aws_vpc.one.id
 }
 
 resource "aws_route_table" "four" {
-tags = {
-Name = "terraform-routetable" 
-}
-vpc_id = aws_vpc.one.id
+  tags = {
+    Name = "terraform-routetable"
+  }
+  vpc_id = aws_vpc.one.id
 
-route {
-cidr_block = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.three.id
-}
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.three.id
+  }
 }
 
 resource "aws_route_table_association" "five" {
-subnet_id = aws_subnet.two.id
-route_table_id = aws_route_table.four.id
+  subnet_id      = aws_subnet.two.id
+  route_table_id = aws_route_table.four.id
 }
 
